03813b414b222942cd0110862fafe5a3fdbbbfd4
xperseguers <xperseguers@735d13b6-9817-0410-8766-e36946ffe9aa>
2011-08-30 08:45:00 +0000

    [TASK] Update checksums
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/xliff/trunk@51521 735d13b6-9817-0410-8766-e36946ffe9aa

--- ext_emconf.php
+++ ext_emconf.php
@@ -1,13 +1,13 @@
 <?php
 
 ########################################################################
-# Extension Manager/Repository config file for ext: "xliff"
+# Extension Manager/Repository config file for ext "xliff".
 #
-# Auto generated 26-08-2011 10:49
+# Auto generated 30-08-2011 10:44
 #
 # Manual updates:
-# Only the data in the array - anything else is removed by next write.
-# "version" and "dependencies" must not be touched!
+# Only the data in the array - everything else is removed by next
+# writing. "version" and "dependencies" must not be touched!
 ########################################################################
 
 $EM_CONF[$_EXTKEY] = array(
@@ -33,7 +33,7 @@ $EM_CONF[$_EXTKEY] = array(
 	'version' => '1.0.0',
 	'constraints' => array(
 		'depends' => array(
-			'php' => '5.2.0-',
+			'php' => '5.2.0-0.0.0',
 			'typo3' => '4.4.0-4.6.99',
 		),
 		'conflicts' => array(
@@ -41,7 +41,7 @@ $EM_CONF[$_EXTKEY] = array(
 		'suggests' => array(
 		),
 	),
-	'_md5_values_when_last_written' => '',
+	'_md5_values_when_last_written' => 'a:8:{s:9:"ChangeLog";s:4:"b288";s:28:"class.tx_xliff_converter.php";s:4:"64f5";s:16:"ext_autoload.php";s:4:"fcb1";s:12:"ext_icon.gif";s:4:"3425";s:17:"ext_localconf.php";s:4:"5007";s:14:"doc/manual.sxw";s:4:"634b";s:41:"xclass/class.ux_sc_mod_tools_em_index.php";s:4:"20d0";s:33:"xclass/class.ux_tx_em_install.php";s:4:"3989";}',
 	'suggests' => array(
 	),
 );
