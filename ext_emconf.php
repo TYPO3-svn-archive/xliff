d68bbfc5ba54ddaa1d377a92a4e8db68b7480a7a
xperseguers <xperseguers@735d13b6-9817-0410-8766-e36946ffe9aa>
2011-08-30 08:41:59 +0000

    [TASK] Add upper bound for TYPO3 version requirement
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/xliff/trunk@51520 735d13b6-9817-0410-8766-e36946ffe9aa

--- ext_emconf.php
+++ ext_emconf.php
@@ -34,7 +34,7 @@ $EM_CONF[$_EXTKEY] = array(
 	'constraints' => array(
 		'depends' => array(
 			'php' => '5.2.0-',
-			'typo3' => '4.4.0-0.0.0',
+			'typo3' => '4.4.0-4.6.99',
 		),
 		'conflicts' => array(
 		),
