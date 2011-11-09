e10a3017006bbbc1bf1ef7c0923f8e05d9aea1e2
xperseguers <xperseguers@735d13b6-9817-0410-8766-e36946ffe9aa>
2011-11-09 15:02:45 +0000

    [TASK] Update checksums
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/xliff/trunk@53956 735d13b6-9817-0410-8766-e36946ffe9aa

--- ext_emconf.php
+++ ext_emconf.php
@@ -3,7 +3,7 @@
 ########################################################################
 # Extension Manager/Repository config file for ext "xliff".
 #
-# Auto generated 30-08-2011 10:44
+# Auto generated 09-11-2011 16:02
 #
 # Manual updates:
 # Only the data in the array - everything else is removed by next
@@ -41,7 +41,7 @@ $EM_CONF[$_EXTKEY] = array(
 		'suggests' => array(
 		),
 	),
-	'_md5_values_when_last_written' => 'a:8:{s:9:"ChangeLog";s:4:"b288";s:28:"class.tx_xliff_converter.php";s:4:"64f5";s:16:"ext_autoload.php";s:4:"fcb1";s:12:"ext_icon.gif";s:4:"3425";s:17:"ext_localconf.php";s:4:"5007";s:14:"doc/manual.sxw";s:4:"634b";s:41:"xclass/class.ux_sc_mod_tools_em_index.php";s:4:"20d0";s:33:"xclass/class.ux_tx_em_install.php";s:4:"3989";}',
+	'_md5_values_when_last_written' => 'a:8:{s:9:"ChangeLog";s:4:"cf3a";s:28:"class.tx_xliff_converter.php";s:4:"387a";s:16:"ext_autoload.php";s:4:"fcb1";s:12:"ext_icon.gif";s:4:"3425";s:17:"ext_localconf.php";s:4:"5007";s:14:"doc/manual.sxw";s:4:"5bbd";s:41:"xclass/class.ux_sc_mod_tools_em_index.php";s:4:"2fa3";s:33:"xclass/class.ux_tx_em_install.php";s:4:"e1c2";}',
 	'suggests' => array(
 	),
 );
