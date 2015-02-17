64c3cbbbbecc9ed2048bc0b637a9a7b7d976054c
xperseguers <xperseguers@735d13b6-9817-0410-8766-e36946ffe9aa>
2015-02-17 08:44:12 +0000

    [TASK] Raise version to 1.0.2 (obsolete)
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/xliff/trunk@88376 735d13b6-9817-0410-8766-e36946ffe9aa

--- ext_emconf.php
+++ ext_emconf.php
@@ -1,14 +1,14 @@
 <?php
 
-########################################################################
-# Extension Manager/Repository config file for ext "xliff".
-#
-# Auto generated 09-11-2011 16:02
-#
-# Manual updates:
-# Only the data in the array - everything else is removed by next
-# writing. "version" and "dependencies" must not be touched!
-########################################################################
+/***************************************************************
+ * Extension Manager/Repository config file for ext "xliff".
+ *
+ * Auto generated 17-02-2015 09:43
+ *
+ * Manual updates:
+ * Only the data in the array - everything else is removed by next
+ * writing. "version" and "dependencies" must not be touched!
+ ***************************************************************/
 
 $EM_CONF[$_EXTKEY] = array(
 	'title' => 'XLIFF',
@@ -22,7 +22,7 @@ $EM_CONF[$_EXTKEY] = array(
 	'priority' => '',
 	'module' => '',
 	'doNotLoadInFE' => 1,
-	'state' => 'stable',
+	'state' => 'obsolete',
 	'internal' => '',
 	'uploadfolder' => 0,
 	'createDirs' => '',
@@ -30,10 +30,10 @@ $EM_CONF[$_EXTKEY] = array(
 	'clearCacheOnLoad' => 0,
 	'lockType' => '',
 	'author_company' => '',
-	'version' => '1.0.1',
+	'version' => '1.0.2',
 	'constraints' => array(
 		'depends' => array(
-			'php' => '5.2.0-0.0.0',
+			'php' => '5.2.0-5.3.99',
 			'typo3' => '4.4.0-4.6.99',
 		),
 		'conflicts' => array(
@@ -41,7 +41,7 @@ $EM_CONF[$_EXTKEY] = array(
 		'suggests' => array(
 		),
 	),
-	'_md5_values_when_last_written' => 'a:8:{s:9:"ChangeLog";s:4:"cf3a";s:28:"class.tx_xliff_converter.php";s:4:"387a";s:16:"ext_autoload.php";s:4:"fcb1";s:12:"ext_icon.gif";s:4:"3425";s:17:"ext_localconf.php";s:4:"5007";s:14:"doc/manual.sxw";s:4:"5bbd";s:41:"xclass/class.ux_sc_mod_tools_em_index.php";s:4:"2fa3";s:33:"xclass/class.ux_tx_em_install.php";s:4:"e1c2";}',
+	'_md5_values_when_last_written' => 'a:8:{s:9:"ChangeLog";s:4:"843d";s:28:"class.tx_xliff_converter.php";s:4:"387a";s:16:"ext_autoload.php";s:4:"fcb1";s:12:"ext_icon.gif";s:4:"3425";s:17:"ext_localconf.php";s:4:"5007";s:14:"doc/manual.sxw";s:4:"5bbd";s:41:"xclass/class.ux_sc_mod_tools_em_index.php";s:4:"2fa3";s:33:"xclass/class.ux_tx_em_install.php";s:4:"e1c2";}',
 	'suggests' => array(
 	),
 );
